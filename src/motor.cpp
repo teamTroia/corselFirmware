8c68a8c8cd364c5a19ab0ec11d56069250faf476
sahvieirag <sabrinavieiraguerra@gmail.com>
2022-11-13 15:50:45 -0300

    de novo

--- src/motor.cpp
+++ src/motor.cpp
@@ -41,3 +41,102 @@ void Motor::setMotors(float vel1, float vel2) {
   setMotorB(vel2);
 
 }
+
+
+// aqui começa a outra palhaçada
+#define SD1_PKS1 = PA0;
+#define IN1_PKS1 = PA0;
+#define SD2_PKS1 = PA0;
+#define IN2_PKS1 = PA0;
+
+#define SD1_PKS2 = PA0;
+#define IN1_PKS2 = PA0;
+#define SD2_PKS2 = PA0;
+#define IN2_PKS2 = PA0;
+
+#define LED_OUT = PA0;
+
+void init_pinout(){
+    pinMode(SD1_PKS1, PWM);
+    pinMode(SD2_PKS1, PWM);
+    pinMode(SD1_PKS2, PWM);
+    pinMode(SD2_PKS2, PWM);
+
+    pinMode(IN1_PKS1, OUTPUT);
+    pinMode(IN2_PKS1, OUTPUT);
+    pinMode(IN1_PKS2, OUTPUT);
+    pinMode(IN2_PKS2, OUTPUT);
+
+    pinMode(LED_OUT, OUTPUT);
+
+    pwmWrite(SD1_PKS1, 0);
+    pwmWrite(SD2_PKS1, 0);
+    pwmWrite(SD1_PKS2, 0);
+    pwmWrite(SD2_PKS2, 0);
+
+    digitalWrite(IN1_PKS1, LOW);
+    digitalWrite(IN2_PKS1, LOW);
+    digitalWrite(IN1_PKS2, LOW);
+    digitalWrite(IN2_PKS2, LOW);
+
+    digitalWrite(LED_OUT, LOW);
+
+}
+
+void enablePKS1(double dutyCycle, int direction){
+    if(direction == 1){
+        pwmWrite(SD1_PKS1, dutyCycle);
+        pwmWrite(SD2_PKS1, 100);
+        digitalWrite(IN1_PKS1, HIGH);
+        digitalWrite(IN2_PKS1, LOW);
+
+    }else if(direction == -1){
+        pwmWrite(SD2_PKS1, dutyCycle);
+        pwmWrite(SD1_PKS1, 100);
+        digitalWrite(IN1_PKS1, LOW);
+        digitalWrite(IN2_PKS1, HIGH);
+
+    }else{
+        pwmWrite(SD2_PKS1, 0);
+        pwmWrite(SD1_PKS1, 0);
+        digitalWrite(IN1_PKS1, LOW);
+        digitalWrite(IN2_PKS1, LOW);
+
+    }
+}
+
+void enablePKS2(double dutyCycle, int direction){
+    if(direction == 1){
+        pwmWrite(SD1_PKS2, dutyCycle);
+        pwmWrite(SD2_PKS2, 100);
+        digitalWrite(IN1_PKS2, HIGH);
+        digitalWrite(IN2_PKS2, LOW);
+
+    }else if(direction == -1){
+        pwmWrite(SD2_PKS2, dutyCycle);
+        pwmWrite(SD1_PKS2, 100);
+        digitalWrite(IN1_PKS2, LOW);
+        digitalWrite(IN2_PKS2, HIGH);
+
+    }else{
+        pwmWrite(SD2_PKS2, 0);
+        pwmWrite(SD1_PKS2, 0);
+        digitalWrite(IN1_PKS2, LOW);
+        digitalWrite(IN2_PKS2, LOW);
+    }
+}
+
+void blinkLED(){
+    digitalWrite(LED_OUT, HIGH);
+    delay(0.5);
+    digitalWrite(LED_OUT, LOW);
+    delay(0.5);
+    digitalWrite(LED_OUT, HIGH);
+    delay(0.5);
+    digitalWrite(LED_OUT, LOW);
+    delay(0.5);
+    digitalWrite(LED_OUT, HIGH);
+    delay(0.5);
+    digitalWrite(LED_OUT, LOW);
+    delay(0.5);
+}
