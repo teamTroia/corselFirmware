53a3c8728ecf00050da70fd4584b07686fac8b59
Rodrigo Marques Duarte <93834250+Zoiudp@users.noreply.github.com>
2023-06-29 16:17:24 -0300

    ToInt()teste

--- src/main.cpp
+++ src/main.cpp
@@ -4,54 +4,38 @@
 #include <Serial.h>
 #include <giroscopio.h>
 #include <Controle.h>
-//#include <Odometria.h>
-
-int leitura, eixoX;
-void setup(){
+#include <Odometria.h>
+int leitura, eixoX, valEixoX;
+void setup()
+{
   pinMode(LED_BUILTIN, OUTPUT);
   initPKS();
-  Serial.begin(9600, SERIAL_8N2);
-  Serial.setTimeout(1000);
+  Serial.begin(9600);
   init_mpu();
 }
 
-void loop(){
-  #if (Serial.available() > 0){
-  #  char c = Serial.read(); // Lê o próximo caractere disponível na porta serial
-  #  if (c == 'b'){
-  #    while (Serial.available() > 0 && Serial.read() != 'b')
-  #    {
-  #      // Continua lendo até encontrar a segunda letra 'b'
-  #    }
-  #  }
-  #  else if (isdigit(c)){
-  #    String receivedString = String(c);
-  #    while (Serial.available() > 0 && Serial.peek() != 'b' && isdigit(Serial.peek()))
-  #    {
-  #      receivedString += char(Serial.read());
-  #    }
+void loop()
+{
+ if (Serial.available()>0)
+  {
+    String palavra = "";
+    char c = ' ';
+    if (Serial.available() > 0) {
+       c = char(Serial.read());
+    }
+    palavra += c;
+    while(c != '\n'){
+      if (Serial.available() > 0) {
+      c = char(Serial.read());
+      //Serial.println(c);
+      palavra = palavra + c;
+      }
+    }
+    int eixoX = palavra.toInt();
 
-  #    leitura = receivedString.toInt();
-  #    Serial.println(leitura, DEC);
-
-    
-    String receivedString = Serial.readStringUntil('b');
-    String numberString = receivedString.substring(1);
-    leitura = numberString.toInt();
-    Serial.println(leitura, DEC);
-      
-    if (leitura > 330){
-      eixoX = -leitura;
-    }
-    else if (leitura < 310){
-      eixoX = leitura;
-    }
-    else{
-      eixoX = 0;
-    }
+    //Serial.println("EixoX: ");
+    //Serial.println(eixoX);
     testeMotor(eixoX);
   }
-
-  // getPosicao(ENCDIR,positionDir,newPosDir);
-  // getPosicao(ENCESC,positionEsq,newPosEsq);
 }
+
