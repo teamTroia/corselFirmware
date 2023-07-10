042ab914f62ab33df457f36affc8793e10c23ad0
Ricardo Piero Lippoli Batista <66374159+Lipolly@users.noreply.github.com>
2023-07-10 16:23:24 -0300

    Update Ultrassonico.h

--- src/Ultrassonico.h
+++ src/Ultrassonico.h
@@ -1,11 +1,12 @@
 #include <defines.h>
-// defines variables
-long duration;
-int distance;
+long duracao;
+int distancia;
+
 void initUltrassonico() {
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
 }
+
 int distCone() {
   // Clears the trigPin
   digitalWrite(trigPin, LOW);
@@ -15,11 +16,11 @@ int distCone() {
   //delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
   // Reads the echoPin, returns the sound wave travel time in microseconds
-  duration = pulseIn(echoPin, HIGH);
+  duracao = pulseIn(echoPin, HIGH);
   // Calculating the distance
-  distance = duration * 0.034 / 2;
+  distancia = duracao * 0.034 / 2;
   // Prints the distance on the Serial Monitor
-  Serial.print("Distance: ");
-  Serial.println(distance);
-  return distance;
+  Serial.print("Distancia: ");
+  Serial.println(distancia);
+  return distancia;
 }
