4f4f7f8d39be5307a662892042465c62c7dd715f
Rodrigo Marques Duarte <93834250+Zoiudp@users.noreply.github.com>
2023-07-24 11:14:42 -0300

    GiroscopioDeu

--- src/Ultrassonico.h
+++ src/Ultrassonico.h
@@ -2,28 +2,30 @@
 long duracao;
 int distancia;
 
-void initUltrassonico() {
+void initUltrassonico()
+{
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
 }
 
-int distCone() {
+int distCone()
+{
   // Clears the trigPin
   digitalWrite(trigPin, LOW);
-  //delayMicroseconds(2);
-  // Sets the trigPin on HIGH state for 10 micro seconds
+  // delayMicroseconds(2);
+  //  Sets the trigPin on HIGH state for 10 micro seconds
   digitalWrite(trigPin, HIGH);
-  //delayMicroseconds(10);
+  // delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
   // Reads the echoPin, returns the sound wave travel time in microseconds
   duracao = pulseIn(echoPin, HIGH);
   // Calculating the distance
   distancia = duracao * 0.034 / 2;
   // Prints the distance on the Serial Monitor
-  
-  if(distance >= 5){
+  if (distancia > 5)
+  {
     Serial.print("Distance: ");
-    Serial.println(distance);
-    return distance;
-  } 
+    Serial.println(distancia);
+    return distancia;
+  }
 }
