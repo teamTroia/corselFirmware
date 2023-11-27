0ef27635d530d89c9d7e97ec9bab987d9fbdab59
Ricardo Piero Lippoli Batista <66374159+Lipolly@users.noreply.github.com>
2023-11-27 11:32:21 -0300

    Update motor.h

--- src/motor.h
+++ src/motor.h
@@ -1,22 +1,22 @@
+//importando bibliotecas
 #ifndef MOTOR_H
 #define MOTOR_H
-
 #include <defines.h>
 #include <giroscopio.h>
 #include <servo.h>
 Servo FW_PKS, ANG_PKS;
 
-void initPKS() {
+void initPKS(){ //função de iniciar o motor através da placa parkinson
         FW_PKS.attach(fw_pks);
         ANG_PKS.attach(ang_pks);
 }
 
-void stopPKS() {
+void stopPKS(){ //função para parar o motor através da placa parkinson
     FW_PKS.writeMicroseconds(1500);
     ANG_PKS.writeMicroseconds(1500);
 }
 
-void motorsControl() {
+void motorsControl(){ //função de simulçao para controle do motor
     if (Serial.available()){
         if(Serial.read() == 'Direita'){
             FW_PKS.writeMicroseconds(2000);
