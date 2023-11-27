62ba3d293514d6022ed9f4c86acee10dc5ccceef
Ricardo Piero Lippoli Batista <66374159+Lipolly@users.noreply.github.com>
2023-11-27 11:21:03 -0300

    Update Ultrassonico.h
    comentando

--- src/Ultrassonico.h
+++ src/Ultrassonico.h
@@ -1,28 +1,27 @@
+//Importa bibliotecas
 #include <defines.h>
 #include <Controle.h>
-// defines variables
+
 long duration;
 int distance;
-void initUltrassonico() {
+void initUltrassonico(){ //função para iniciar ultrassonico
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
 }
-void distCone() {
-  // Clears the trigPin
+void distCone(){ //função para saber a distancia do robô ao cone
+  // limpando trigPin
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
-  // Sets the trigPin on HIGH state for 10 micro seconds
+  // setando o trigPin como estado HIGH por 10 micro segundos
   digitalWrite(trigPin, HIGH);
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
-  // Reads the echoPin, returns the sound wave travel time in microseconds
+  // faz a leitura do echoPin, retorna o pulso do tempo
   duration = pulseIn(echoPin, HIGH);
-  // Calculating the distance
+  // calcula a distancia
   distance = duration * 0.034 / 2;
-  // Prints the distance on the Serial Monitor
-  Serial.print("Distance: ");
-  Serial.println(distance);
-  if(distance <= 300){
+  
+  if(distance <= 300){ //se a distancia for menor que 300 para o robô e pisca o LED
     FW_PKS.writeMicroseconds(1500);
     ANG_PKS.writeMicroseconds(1500);
     pinMode(LEDCONE,LOW);
