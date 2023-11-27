1359d5c75a10d91605d88bfe97f8c3b1dfa64eff
Ricardo Piero Lippoli Batista <66374159+Lipolly@users.noreply.github.com>
2023-11-27 11:24:00 -0300

    Update Serial.h
    comentando

--- src/Serial.h
+++ src/Serial.h
@@ -1,38 +1,27 @@
+//Importa bibliotecas
 #include <Arduino.h>
 
-// Colocar o baudrate desejado para leitura
-int SerialConvertion()
-{
+int SerialConvertion(){ //função que faz a descriptografia das cordenadas recebidas pelo serial
     String palavra = " ";
     char c;
-    if (Serial.available() > 0)
-    {
+    if (Serial.available() > 0){ //se não existir valor no Serial, retorna 0
         c = (Serial.read());
-        if (c == (','))
-        {
-            while (c != '/')
-            {
+        if (c == (',')){
+            while (c != '/'){
                 int i = 0;
-                if (Serial.available() > 0)
-                {   
+                if (Serial.available() > 0){   
                     c = char(Serial.read());
                     i++;
-                    if (i < 2)
-                    {
-                        
-                        Serial.println(c);
+                    if (i < 2){
                         palavra = palavra + c;
                     }
                 }
             }
-            Serial.println(palavra);
             int eixoX = palavra.toInt();
-            Serial.println(eixoX);
             return eixoX;
         }
     }
-    else
-    {
+    else{
         return 0;
     }
 }
