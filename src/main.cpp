315764460ae463a714dd858227b226bdb2930638
Ricardo Piero Lippoli Batista <66374159+Lipolly@users.noreply.github.com>
2023-11-27 11:15:27 -0300

    Update main.cpp
    comentando

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,11 @@
+//Importar bibliotecas
 #include <Arduino.h>
-// #include <defines.h>
-// #include <motor.h>
 #include <Serial.h>
-// #include <giroscopio.h>
 #include <Controle.h>
-//#include <Odometria.h>
 #include <Ultrassonico.h>
 
 int leitura, eixoX, valEixoX;
-void setup()
-{
+void setup(){ //função de inicialização dos sensores
   pinMode(LED_BUILTIN, OUTPUT);
   initPKS();
   Serial.begin(9600);
@@ -17,11 +13,8 @@ void setup()
   initUltrassonico();
 }
 
-void loop()
-{
+void loop(){ //função principal
   eixoX = SerialConvertion();
   testeMotor(eixoX);
   distCone();
-  
-  //getpulse(1900,1500);
 }
